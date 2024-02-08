91abc4c7cb2e1015ecd7bc3f1c4416ad95615f01
Ryan-T-Orth <135889335+Ryan-T-Orth@users.noreply.github.com>
2024-02-07 22:12:50 -0700

    Tenatively Complete

--- Person.java
+++ Person.java
@@ -1,6 +1,37 @@
 public class Person {
 
     private int wage;
+    private int wealth;
     private boolean thief;
     
+    public Person(int wage) {
+        this.wage = wage;
+        this.thief = false;
+        this.wealth = 0;
+    }
+
+    public int getWealth() {
+        return this.wealth;
+    }
+
+    public boolean isThief() {
+        return this.thief;
+    }
+
+    public void setThief() {
+        this.thief = true;
+    }
+
+    public void chaseTheBag() {
+        this.wealth += this.wage;
+    }
+
+    public void getRobbed() {
+        this.wealth /= 2;
+    }
+
+    public void steal(Person p) {
+        this.wealth += (p.getWealth() / 2);
+        p.getRobbed();
+    }
 }
